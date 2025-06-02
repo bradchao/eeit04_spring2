6edc572f651fcb308e1aec65209fa3f3ceac7298
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-02 10:55:04 +0800

    06021055

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Member;
 import com.example.demo.service.MemberService;
 
 @RestController
@@ -19,6 +25,16 @@ public class MemberController {
 	@GetMapping("/exists")
 	public ResponseEntity<Boolean> checkAccount(@RequestParam String account) {
 		return ResponseEntity.ok(memberService.checkAccount(account));
+	}
+	
+	/*
+	 * Response: {"message":"success"}
+	 */
+	@PostMapping("/register")
+	public ResponseEntity<Map<String, String>> register(@RequestBody Member member){
+		HashMap<String, String> response = new HashMap<>();
+		response.put("message", "success");
+		return ResponseEntity.ok(response);
 	}
 
 }
